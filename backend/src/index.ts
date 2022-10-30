04c14c93314ba2b3e32e48c919bc16f4436a5ec7
Kostya Kosyuk <80011474+Kostya7337@users.noreply.github.com>
2022-10-30 11:17:03 +0200

    husky check

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,8 +4,10 @@ const PORT = 3000;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send("Hello World");
+  res.status(200).send('Hello World');
 });
+
+decm
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
