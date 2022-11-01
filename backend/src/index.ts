f13b764924d8c05aa63aee92f48a14c070f73acd
Kostya Kosyuk <80011474+Kostya7337@users.noreply.github.com>
2022-11-01 13:44:23 +0200

    used "path.join", dist removed from commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,21 @@
 'use strict';
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import express from 'express';
 import cors from 'cors';
 import { productRouter } from './routers/productRouter.js';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 const PORT = 3000;
 const app = express();
 
 app.use(cors());
+app.use('/public', express.static(path.join(__dirname + '/public')));
 app.use('/product', express.json(), productRouter);
 
 app.listen(process.env.PORT || PORT, () => {
