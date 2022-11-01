980707e2db52485c59612d044caefc545619a87c
Kostya Kosyuk <80011474+Kostya7337@users.noreply.github.com>
2022-11-01 21:37:58 +0200

    added range arguments to getPhone(from, to), added sortType to getPhone(sortType: SortType), you can use them together

--- frontend/src/api/phone.ts
+++ frontend/src/api/phone.ts
@@ -1,11 +1,24 @@
+import { Params } from '../types/params';
 import { Phone } from '../types/phone';
+import { SortTypes } from '../types/sortTypes';
 
 import { client } from './fetchClient';
 
 const productEndPoint = '/product';
 
-export const getPhones = async () => {
-  const phones = await client.get<Phone[]>(productEndPoint);
+export const getPhones = async (
+  from?: number,
+  to?: number,
+  sortType: SortTypes = SortTypes.alphabetically,
+) => {
+  const params: Params = { sort: sortType };
+
+  if (Number.isInteger(from) && Number.isInteger(to)) {
+    params.from = from?.toString();
+    params.to = to?.toString();
+  }
+
+  const phones = await client.get<Phone[]>(productEndPoint, params);
 
   return phones;
 };
