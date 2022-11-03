d9d1756fab1bccc543c74d8bf8b8bf29fb029f7a
Andrii Dmytruk <andriydmitr2014@gmail.com>
2022-11-03 19:37:52 +0200

    Full completed fav page

---
+++ frontend/src/components/FavoritesList/FavoritesList.tsx
@@ -0,0 +1,45 @@
+import React, { useCallback } from 'react';
+import { Card } from '../Card';
+import { Phone } from '../../types/phone';
+import cardList from './FavoritesList.module.scss';
+import { useLocalStorage } from '../../helpers/localStorage';
+import { updatePhonesList } from '../../helpers/updatePhonesList';
+
+type Props = {
+  favorites: Phone[],
+  setFavorites: (prev: Phone[]) => void;
+}
+
+export const FavoritesList = React.memo(function FavoritesList({
+  favorites,
+  setFavorites,
+}: Props) {
+
+  const [cart, setCart] = useLocalStorage('cart', []);
+
+
+  const addItemToCart = useCallback((phone: Phone) => {
+    setCart((prev: Phone[]) => updatePhonesList(prev, phone));
+  }, [cart]);
+
+  const addItemToFavorites = useCallback((phone: Phone) => {
+    setFavorites(updatePhonesList(favorites, phone));
+  }, [favorites]);
+
+  return (
+    <>
+      <div className={cardList.CardList}>
+        {favorites.map((phone: Phone) => (
+          <Card
+            key={phone.id}
+            phone={phone}
+            addItemToCart={addItemToCart}
+            addItemToFavorites={addItemToFavorites}
+            cart={cart}
+            favorites={favorites}
+          />
+        ))}
+      </div>
+    </>
+  );
+});
--- frontend/src/pages/PageFavorites/PageFavorites.tsx
+++ frontend/src/pages/PageFavorites/PageFavorites.tsx
@@ -1,14 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { CardList } from '../../components/CardList';
-import { setToLocalStorage, useLocalStorage } from '../../helpers/localStorage';
+import { FavoritesList } from '../../components/FavoritesList';
+import { useLocalStorage } from '../../helpers/localStorage';
 import home_icon from '../../img/icons/home-icon.svg';
 import right_arrow_icon from '../../img/icons/right-arrow-icon.svg';
-import { Phone } from '../../types/phone';
 import pageNav from '../PageNav.module.scss';
 
 
-export const PageFavorites = React.memo(function PageNotFound() {
+export const PageFavorites = () => {
   const [favorites, setFavorites] = useLocalStorage('favorites', []);
 
   return (
@@ -29,7 +28,7 @@ export const PageFavorites = React.memo(function PageNotFound() {
         {`${favorites.length} models`}
       </p>
 
-      <CardList data={favorites} />
+      <FavoritesList favorites={favorites} setFavorites={setFavorites} />
     </>
   );
-});
+};
