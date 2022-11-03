32b6a51345bbc7ea95f217157b66530d847fe07f
Andrii Dmytruk <andriydmitr2014@gmail.com>
2022-11-03 20:46:40 +0200

    Fixed when localstorage empty

--- frontend/src/helpers/localStorage.ts
+++ frontend/src/helpers/localStorage.ts
@@ -1,42 +1,12 @@
-// import { useState } from 'react';
-import { Phone } from '../types/phone';
 
-// function setToLocalStorage(
-//   key: string,
-// ) {
-//   if (!localStorage.getItem(key)) {
-//     localStorage.setItem(key, JSON.stringify([]));
-//   }
-// }
+function setToLocalStorage(
+  key: string,
+) {
+  if (!localStorage.getItem(key)) {
+    localStorage.setItem(key, JSON.stringify([]));
+  }
+}
 
-// export function useLocalStorage(key: string, initialValue: Phone[]) {
-//   setToLocalStorage(key);
-//   const [storedValue, setStoredValue] = useState(() => {
-//     if (typeof window === 'undefined') {
-//       return initialValue;
-//     }
-//     try {
-//       const item = window.localStorage.getItem(key);
-//       return item ? JSON.parse(item) : initialValue;
-//     } catch (error) {
-//       console.log(error);
-//       return initialValue;
-//     }
-//   });
-//   const setValue = (value: Phone[]): void => {
-//     try {
-//       const valueToStore =
-//         value instanceof Function ? value(storedValue) : value;
-//       setStoredValue(valueToStore);
-//       if (typeof window !== 'undefined') {
-//         window.localStorage.setItem(key, JSON.stringify(valueToStore));
-//       }
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-//   return [storedValue, setValue];
-// }
 
 import {
   Dispatch,
@@ -57,6 +27,7 @@ declare global {
 type SetValue<T> = Dispatch<SetStateAction<T>>
 
 export function useLocalStorage<T>(key: string, initialValue: T): [T, SetValue<T>] {
+  setToLocalStorage(key);
   // Get from local storage then
   // parse stored json or return initialValue
   const readValue = useCallback((): T => {
--- frontend/src/helpers/updatePhonesList.ts
+++ frontend/src/helpers/updatePhonesList.ts
@@ -1,14 +1,10 @@
 import { Phone } from '../types/phone';
-import { createNotification, NotificationType } from './createNotification';
 
 export const updatePhonesList = (cart: Phone[], phone: Phone): Phone[] => {
   let newCart = cart.filter((el: Phone) => el.id !== phone.id);
 
   if (newCart.length === cart.length) {
     newCart = [...newCart, phone];
-    createNotification(NotificationType.addToCart, phone.name);
-  } else {
-    createNotification(NotificationType.removeFromCart, phone.name);
   }
 
   return newCart;
