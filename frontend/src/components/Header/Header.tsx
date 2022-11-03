51d8ef7e131f0e5cf50dff72cccaa4b1f73e244d
Andrii Dmytruk <andriydmitr2014@gmail.com>
2022-11-03 20:32:17 +0200

    Changed uselocalstorage hook and added icons with cart and favs to the header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import classNames from 'classnames';
 import logo from './logo/full_logo.png';
