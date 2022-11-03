c97c84ded56ce4191067779afb833a08dfcb1662
Andrii Dmytruk <andriydmitr2014@gmail.com>
2022-11-03 17:41:20 +0200

    favorites without deleting

--- frontend/src/pages/PageFavorites/PageFavorites.tsx
+++ frontend/src/pages/PageFavorites/PageFavorites.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { CardList } from '../../components/CardList';
+import { setToLocalStorage, useLocalStorage } from '../../helpers/localStorage';
 import home_icon from '../../img/icons/home-icon.svg';
 import right_arrow_icon from '../../img/icons/right-arrow-icon.svg';
+import { Phone } from '../../types/phone';
 import pageNav from '../PageNav.module.scss';
 
 
 export const PageFavorites = React.memo(function PageNotFound() {
+  const [favorites, setFavorites] = useLocalStorage('favorites', []);
+
   return (
     <>
       <div className={pageNav.navInfo}>
@@ -21,8 +26,10 @@ export const PageFavorites = React.memo(function PageNotFound() {
         Favorites
       </h1>
       <p className={pageNav.modelsCount}>
-        {`${10} models`}
+        {`${favorites.length} models`}
       </p>
+
+      <CardList data={favorites} />
     </>
   );
 });
